ff495b161a1a9ea03a67d9fafbd1f8c31bf4f015
Jacob Marble <jacobmarble@google.com>
2015-05-21 17:12:27 -0700

    Improve lib.Semaphore.

--- lib/semaphore.go
+++ lib/semaphore.go
@@ -8,26 +8,47 @@ https://developers.google.com/open-source/licenses/bsd
 package lib
 
 type Semaphore struct {
-	ch chan bool
+	ch chan struct{}
 }
 
 func NewSemaphore(size uint) *Semaphore {
-	s := make(chan bool, size)
+	s := make(chan struct{}, size)
 	return &Semaphore{s}
 }
 
+// Acquire increments the semaphore, blocking if necessary.
 func (s *Semaphore) Acquire() {
-	s.ch <- true
+	s.ch <- struct{}{}
 }
 
+// TryAcquire increments the semaphore without blocking.
+// Returns false if the semaphore was not acquired.
+func (s *Semaphore) TryAcquire() bool {
+	select {
+	case s.ch <- struct{}{}:
+		return true
+	default:
+		return false
+	}
+}
+
+// Release decrements the semaphore. If this operation causes
+// the semaphore value to be negative, then panics.
 func (s *Semaphore) Release() {
-	<-s.ch
+	select {
+	case _ = <-s.ch:
+		return
+	default:
+		panic("Semaphore was released without being acquired")
+	}
 }
 
+// Count returns the current value of the semaphore.
 func (s *Semaphore) Count() uint {
 	return uint(len(s.ch))
 }
 
+// Size returns the maximum semaphore value.
 func (s *Semaphore) Size() uint {
 	return uint(cap(s.ch))
 }
