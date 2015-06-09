a599965d126044affd6d27e0c0626fc4ab1efd8a
Jacob Marble <jacobmarble@google.com>
2015-06-09 16:47:38 -0700

    Implemented Privet token generation and validation.

---
+++ privet/xsrf.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2015 Google Inc. All rights reserved.
+
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file or at
+https://developers.google.com/open-source/licenses/bsd
+*/
+
+package privet
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/base64"
+	"time"
+)
+
+const (
+	deviceSecretLength = 24             // 24 bytes == 192 bits
+	tokenTimeout       = 24 * time.Hour // Specified in GCP Privet doc.
+)
+
+// xsrf generates and validates XSRF tokens.
+type xsrf struct {
+	deviceSecret []byte
+}
+
+func newXSRF() *xsrf {
+	// Generate a random device secret.
+	deviceSecret := make([]byte, deviceSecretLength)
+	rand.Read(deviceSecret)
+	return &xsrf{deviceSecret}
+}
+
+func (x *xsrf) newToken() string {
+	t := time.Now()
+	return x.newTokenProvideTime(t)
+}
+
+func (x *xsrf) newTokenProvideTime(t time.Time) string {
+	tb := int64ToBytes(t.Unix())
+	sum := sha1.Sum(append(x.deviceSecret, tb...))
+	token := append(sum[:], tb...)
+	return base64.StdEncoding.EncodeToString(token)
+}
+
+func (x *xsrf) isTokenValid(token string) bool {
+	return x.isTokenValidProvideTime(token, time.Now())
+}
+
+func (x *xsrf) isTokenValidProvideTime(token string, now time.Time) bool {
+	tokenBytes, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		return false
+	}
+
+	tb := tokenBytes[sha1.Size:]
+	t := time.Unix(bytesToInt64(tb), 0)
+	if now.Sub(t) > tokenTimeout {
+		return false
+	}
+	if t.Sub(now) > 0 {
+		return false
+	}
+
+	sum := sha1.Sum(append(x.deviceSecret, tb...))
+	if 0 != bytes.Compare(sum[:], tokenBytes[:sha1.Size]) {
+		return false
+	}
+
+	return true
+}
+
+func int64ToBytes(v int64) []byte {
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = byte(v >> uint(8*i))
+	}
+	return b
+}
+
+func bytesToInt64(b []byte) int64 {
+	if len(b) != 8 {
+		return 0
+	}
+	var v int64
+	for i := range b {
+		v |= int64(b[i]) << uint(8*i)
+	}
+	return v
+}
