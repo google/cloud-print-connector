4f8ce2d8156771f7c030e92e5aedccb5f72ece7a
Jacob Marble <jacobmarble@google.com>
2015-09-25 15:28:04 -0700

    Some GoDoc cleanup.

--- gcp/job.go
+++ gcp/job.go
@@ -5,6 +5,7 @@ Use of this source code is governed by a BSD-style
 license that can be found in the LICENSE file or at
 https://developers.google.com/open-source/licenses/bsd
 */
+
 package gcp
 
 import "github.com/google/cups-connector/cdd"
