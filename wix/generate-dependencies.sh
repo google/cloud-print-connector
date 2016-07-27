f84bb043db41f178beb1a2c949d11a980f40afc4
arastooz <arastooz@users.noreply.github.com>
2016-07-27 15:39:18 -0700

    Windows installer improvements. (#276)
    Move manual init cmd to before ExecuteAction in InstallUISequence to make it work properly.
    Move dependencies out to separate file and add script to update dependencies.
    Allow for setting config file path during install instead of running init

---
+++ wix/generate-dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+echo '<Wix xmlns="http://schemas.microsoft.com/wix/2006/wi">'>dependencies.wxs
+echo '<Fragment>
+    <!-- Group of components of all the dependency dlls -->
+    <ComponentGroup
+        Id="Dependencies"
+        Directory="INSTALLFOLDER"
+        Source="!(wix.DependencyDir)">'>>dependencies.wxs
+for f in `ldd ${GOPATH}/bin/gcp-windows-connector.exe | grep -v Windows | sed s/" =>.*"// | sed s/"\t"//`
+  do echo "      <Component>
+        <File Name=\"$f\" KeyPath=\"yes\"/>
+      </Component>">>dependencies.wxs; done
+echo '    </ComponentGroup>
+  </Fragment>
+</Wix>'>>dependencies.wxs
+
